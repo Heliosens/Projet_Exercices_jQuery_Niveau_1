4ffdf369ed43a30129b58b89b42e57bcf4c4b007
Sylvie-B <unsolo@hotmail.fr>
2021-12-20 16:40:42 +0100

    select children

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 $('#myId').text("Hello World");
 $('a').attr('href', 'https://learn.jquery.com/using-jquery-core/attributes/');
+let bigDiv = $('div');
+bigDiv.css({'height' : '10vh', 'width' : '5vw', 'border' : '1px dashed black'});
+bigDiv.children('div').get(1)
