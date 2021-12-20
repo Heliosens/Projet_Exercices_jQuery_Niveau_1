9abec2facb9baed51c36c71816d9409acdc94ca2
Sylvie-B <unsolo@hotmail.fr>
2021-12-20 19:47:02 +0100

    parent children

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $('#myId').text("Hello World");
 $('a').attr('href', 'https://learn.jquery.com/using-jquery-core/attributes/');
-let bigDiv = $('div');
-bigDiv.css({'height' : '10vh', 'width' : '5vw', 'border' : '1px dashed black'});
-bigDiv.children('div').get(1)
+let bigDiv = $('#parent');
+
+bigDiv.children().css({'height' : '10vh', 'width' : '15vw', 'border' : '1px dashed black'});
+
+bigDiv.children().get(1).append('ceci est un texte');
